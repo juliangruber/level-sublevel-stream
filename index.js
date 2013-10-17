7beb182a328d40bca33886ab42b2a2690b5fab60
Julian Gruber <julian@juliangruber.com>
2013-10-17 22:46:31 +0200

    git rid of logic for start / end

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ module.exports = function(db, opts) {
 
   if (opts.gt) opts.gt = sep + opts.gt + sep + sep;
   else if (opts.gte) opts.gte = sep + opts.gte;
-  else if (opts.start) opts.start = sep + opts.start;
   else opts.gt = sep;
 
   if (opts.lt) opts.lt = sep + opts.lt;
   else if (opts.lte) opts.lte = sep + opts.lte + sep + sep;
-  else if (opts.end) opts.end = sep + opts.end;
   else opts.lt = sep + sep;
 
   opts.limit = 1;
@@ -20,7 +18,6 @@ module.exports = function(db, opts) {
   (function next() {
     var found = false;
 
-    console.log('opts', opts);
     db.createKeyStream(opts)
     .on('error', function(err) {
       tr.emit('error', err);
@@ -32,10 +29,10 @@ module.exports = function(db, opts) {
 
       if (opts.reverse) {
         opts.lt = sep + sub;
-        opts.end = opts.lte = null;
+        opts.lte = null;
       } else {
         opts.gt = sep + sub + sep + sep;
-        opts.start = opts.gte = null;
+        opts.gte = null;
       }
 
       next();
